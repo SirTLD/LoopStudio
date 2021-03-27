982f0e9dbde595718dee504b338040015baa7dca
SirTLD <ekanemita@gmail.com>
2021-03-27 02:37:52 -0500

    update to hambruger and nav

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const togglebutton = document.querySelector('.toggle-button');
+const burgerIcon = document.querySelector('.burger');
 
 const navbarLinks = document.querySelector('.nav-links');
 
-togglebutton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
+const navLinkItems = document.querySelectorAll('.nav-links li');
+
+burgerIcon.addEventListener('click', () => {
+  navbarLinks.classList.toggle('nav-active');
+
+  navLinkItems.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `menuSlide 0.5s ease forwards ${index / 5 + 0.6}s`;
+    }
+  });
+
+  burgerIcon.classList.toggle('.toggle');
 });
